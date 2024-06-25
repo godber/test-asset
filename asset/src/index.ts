cd1d46cb409978c60d96cbcc8d9afd03731fa1e4
Austin Godber <godber@uberhip.com>
2024-06-24 17:49:29 -0700

    asset builds

---
+++ asset/src/index.ts
@@ -0,0 +1,9 @@
+import Stdout from './stdout/processor';
+import StdoutSchema from './stdout/schema';
+
+export const ASSETS = {
+    stdout: {
+        Processor: Stdout,
+        Schema: StdoutSchema
+    },
+}
